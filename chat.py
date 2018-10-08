0400e1fced573bee17b625b50a7a35eb308a97fb
Sam Chen <free6323@hotmail.com>
2018-10-08 21:41:34 +0800

    add check and write file

--- chat.py
+++ chat.py
@@ -8,28 +8,28 @@ def read_file(filename):
 		for line in f:
 			chat.append(line.strip())
 	return chat
-# def check_chat(chat):
-# 	for i in chat:
-# 		if i == 'Allen':
-# 			who = i
-# 			continue
-# 		elif i == 'Tom':
-# 			who = i
-# 			continue
-# 		chat_check.append(who + ': ' + i + '\n')
-
-
+def check_chat(chat):
+	for i in chat:
+		if 'Allen' in i:
+			who = i
+			continue
+		elif 'Tom' in i:
+			who = i
+			continue
+		chat_check.append(who + ': ' + i + '\n')
 	return chat_check
-
+# 寫檔案
+def write_file(write):
+	with open(write, 'w', encoding='UTF-8') as keyin:
+		for i in chat_check:
+			keyin.write(i)
 # main
 def main():
 
 	filename = ('input.txt')
+	write = ('output_name.txt')
 	read_file(filename)
 	print(chat)
-	# check_chat(chat)
-
-	with open('test.txt', 'w', encoding='UTF-8') as keyin:
-		for i in chat_check:
-			keyin.write(i)
+	check_chat(chat)
+	write_file(write)
 main()
